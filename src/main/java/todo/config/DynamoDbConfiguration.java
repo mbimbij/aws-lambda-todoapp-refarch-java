10a1eed7324bf4d75a842bd8941ba24e7be6cf56
Your Name <you@example.com>
2020-12-18 07:20:00 +0100

    Perform yet another refactoring. Definitely not the final one though

---
+++ src/main/java/todo/config/DynamoDbConfiguration.java
@@ -0,0 +1,8 @@
+package todo.config;
+
+import com.amazonaws.regions.Regions;
+
+public class DynamoDbConfiguration {
+  public static final String TABLE_NAME = "todo-table";
+  public static final Regions REGION = Regions.EU_WEST_3;
+}
--- src/main/java/todo/repository/TodoItemRepository.java
+++ src/main/java/todo/repository/TodoItemRepository.java
@@ -1,4 +1,17 @@
 package todo.repository;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import todo.config.DynamoDbConfiguration;
+
 public class TodoItemRepository extends AbstractRepository<TodoItemEntity, String> {
+  public static TodoItemRepository create(){
+    AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+        .withRegion(DynamoDbConfiguration.REGION.getName())
+        .build();
+    TodoItemRepository todoItemRepository = new TodoItemRepository();
+    todoItemRepository.setMapper(new DynamoDBMapper(client));
+    return todoItemRepository;
+  }
 }
